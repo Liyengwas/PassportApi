c1854c1bbe5d781e6a6b1422cb0225bddafe6721
Liyengwas <liyengwas@gmail.com>
2019-12-07 18:37:51 +0300

    Initial Commit

---
+++ app/Http/Resources/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Product extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'ProductName' => $this->ProductName,
+            'ProductPrice' => $this->ProductPrice,
+            'ProductDesc' => $this->ProductDesc,
+            'created_at' => $this->created_at->format('d/m/Y'),
+            'updated_at' => $this->updated_at->format('d/m/Y'),
+        ];
+    }
+}
